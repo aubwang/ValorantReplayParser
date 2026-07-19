7baa98b9cb8ebe0c0b979e756cad854da33ddec8
Michel Giehl <gimifehd@gmail.com>
2026-07-19 20:22:22 +0200

    refactor: reduce number of info logs in cli reader

--- src/CliReader/ReplayLogRunner.cs
+++ src/CliReader/ReplayLogRunner.cs
@@ -31,20 +31,15 @@ internal sealed class ReplayLogRunner
             actorEventLogger,
             ParseProfile.Default).Read(archive);
 
-        _logger.LogInformation("Took: {ElapsedMilliseconds}ms", stopwatch.ElapsedMilliseconds);
-        _logger.LogInformation("Read replay {ReplayName}", context.ReplayInfo.FriendlyName);
-        _logger.LogInformation("Version {ReplayVersion}", context.ReplayVersion.Branch);
-        _logger.LogInformation("Chunks {ChunkCount}", context.ReplayInfo.Chunks.Count);
-        _logger.LogInformation("Timestamp {Timestamp}", context.ReplayInfo.Timestamp);
-        _logger.LogInformation("Duration {Duration}", TimeSpan.FromMilliseconds(context.ReplayInfo.LengthInMs));
-        _logger.LogInformation("File Size {FileSize} MB", file.Length / 1_000_000);
-        _logger.LogInformation(
-            "Packet Stats: Bunch Count={BunchCount}\tPacket Count={PacketCount}\tMalformedPacketCount={MalformedPacketCount}\tPartialErrorCount={PartialErrorCount}\tTTL Bytes={TotalBytes} MB",
-            context.PacketStats.BunchCount,
-            context.PacketStats.PacketCount,
-            context.PacketStats.MalformedPacketCount,
-            context.PacketStats.PartialErrorCount,
-            context.PacketStats.TotalPacketBytes / 1_000_000);
+        Console.WriteLine($"Took: {stopwatch.ElapsedMilliseconds}ms");
+        Console.WriteLine($"Read replay {context.ReplayInfo.FriendlyName}");
+        Console.WriteLine($"Version {context.ReplayVersion.Branch}");
+        Console.WriteLine($"Chunks {context.ReplayInfo.Chunks.Count}");
+        Console.WriteLine($"Timestamp {context.ReplayInfo.Timestamp}");
+        Console.WriteLine($"Duration {TimeSpan.FromMilliseconds(context.ReplayInfo.LengthInMs)}");
+        Console.WriteLine($"File Size {file.Length / 1_000_000} MB");
+        Console.WriteLine(
+            $"Packet Stats: Bunch Count={context.PacketStats.BunchCount}\tPacket Count={context.PacketStats.PacketCount}\tMalformedPacketCount={context.PacketStats.MalformedPacketCount}\tPartialErrorCount={context.PacketStats.PartialErrorCount}\tTTL Bytes={context.PacketStats.TotalPacketBytes / 1_000_000} MB");
         actorEventLogger.LogSummary();
     }
 }
