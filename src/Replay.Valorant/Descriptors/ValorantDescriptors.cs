468e189180125a73ed7d7191a38e8f20ad98be3e
Michel Giehl <gimifehd@gmail.com>
2026-07-20 21:27:40 +0200

    fix: Remove duplicate tripwire descriptor entry

--- src/Replay.Valorant/Descriptors/ValorantDescriptors.cs
+++ src/Replay.Valorant/Descriptors/ValorantDescriptors.cs
@@ -86,7 +86,6 @@ public static class ValorantDescriptors
         catalog.Add(new RemoteCharacterUpdateDescriptor());
         catalog.Add(new BaseReplayPlayerState());
         catalog.Add(new BaseReplayControllerDescriptor());
-        catalog.Add(new TripWireAbilityDescriptor());
         catalog.Add(new BaseReplayControllerClassNetCacheDescriptor());
         catalog.Add(new BombGameStateClassNetCacheDescriptor());
         catalog.Add(new AresAbilitySystemComponentClassNetCacheDescriptor());
